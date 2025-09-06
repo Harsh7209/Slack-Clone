f9b18ff74c824ceea70b3dc642c9644fa4450fd7
Harsh Choubey <harshchoubat759@gmail.com>
2025-09-06 23:33:13 +0530

    done

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,9 +1,12 @@
 import { UserButton } from "@clerk/clerk-react";
 
 const HomePage = () => {
-  return <div>
-    <UserButton />
-    Home Page</div>;
+  return (
+    <div>
+      <UserButton />
+      Home Page
+    </div>
+  );
 };
 
 export default HomePage;
